43abf2855870b2af24dfacb033893c2a3c07201b
Mateus Nunes <mateus.2006@gmail.com>
2020-02-27 01:38:13 -0300

    First insert into Product

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,10 +1,9 @@
 const Category = require('../models/Category')
+const Product = require('../models/Product')
 
 module.exports = {
     create(req, res) {
         //pegar categorias
-        console.log('x')
-
         Category.all()
         .then(function(results) {
             const categories = results.rows
@@ -15,8 +14,24 @@ module.exports = {
         })
 
     },
-    post(req, res){
-        // Log
+    async post(req, res){
+        const keys = Object.keys(req.body); /*Retorna apenas as chaves dos campos*/
+    
+        for (key of keys){
+            if (req.body[key] == "")
+              return res.send("Please, fill all fields");
+        }
+
+        let results = await Product.create(req.body)
+        const productId = results.rows[0].id
+        console.log(productId)
+
+        results = await Category.all()
+        const categories = results.rows
+
+
+        return res.render("products/create.njk", { productId, categories })
+
         
 
 
