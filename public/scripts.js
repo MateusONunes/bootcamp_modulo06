af56aeae5d5ffdb47f690c31fc476c9bbc0a0be9
Mateus Nunes <mateus.2006@gmail.com>
2020-03-01 15:28:06 -0300

    before add icon into div of image

--- public/scripts.js
+++ public/scripts.js
@@ -18,34 +18,50 @@ const Mask = {
 /*Mask.app*/
 
 const PhotosUpload = {
+    preview: document.querySelector('#photos-preview'),
     uploadLimit: 6,
-
+    
     handleFileInput(event) {/*Aula Upload de imagens/ 1 - Gerenciador de imagens do Front end/ "Lendo imagens com Java Script no Front end"*/
 
-        const { files: fileList } = event.target
-        const  uploadLimit  = PhotosUpload.uploadLimit
+        const {files: fileList } = event.target
 
-        if (fileList.length > uploadLimit) {
-            alert(`Envie no máximo ${uploadLimit} fotos`)
-            event.preventDefault() // bloqueia o evento
-        }
+        if (PhotosUpload.hasLimit(event, fileList)) return
 
         Array.from(fileList).forEach(file => {
-            console.log('x')
             const reader = new FileReader()
-            
+           
             reader.onload = () => {
                 const image = new Image()
                 image.src = String(reader.result)
 
-                const div = document.createElement('div')
-                div.classList.add('photo')
-                div.onclick = () => alert('remover foto')
-                div.appendChild(image)
-                document.querySelector('#photos-preview').appendChild(div)
+                const div = PhotosUpload.getContainer(image)
+
+
+                PhotosUpload.preview.appendChild(div)
             }
 
             reader.readAsDataURL(file)            
         });
     },
+
+    getContainer(image) {
+        const div = document.createElement('div')
+        div.classList.add('photo')
+        div.onclick = () => alert('remover foto')
+        div.appendChild(image)
+
+        return div
+    },
+    hasLimit(event, fileList) {
+        const  uploadLimit  = PhotosUpload.uploadLimit
+
+        if (fileList.length > uploadLimit) {
+            alert(`Envie no máximo ${uploadLimit} fotos`)
+            event.preventDefault() // bloqueia o evento
+            return true
+        }
+
+        return false
+
+    }
 }
